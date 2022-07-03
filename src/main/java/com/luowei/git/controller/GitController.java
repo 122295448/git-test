b3b26116dbc669cacd476abac22abe4fb34f3c7f
luowei <122295448@qq.com>
2022-07-03 17:01:42 +0800

    push commit

--- src/main/java/com/luowei/git/controller/GitController.java
+++ src/main/java/com/luowei/git/controller/GitController.java
@@ -9,5 +9,6 @@ public class GitController {
         System.out.println("hello 555");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
